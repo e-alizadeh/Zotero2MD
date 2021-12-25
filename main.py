9f9b5eef04cd7dd943a94fd2c005b21b9ccbb7d3
ealizadeh <alizadeh.essi@gmail.com>
2021-12-24 22:18:38 -0500

    Create first draft of a document from a highlights of a single document

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from collections import defaultdict
 from dotenv import load_dotenv
 import os
 from pyzotero.zotero import Zotero
-from dataclass import dataclass
-from typing import List
+# from dataclass import dataclass
+from typing import Dict, List
+
+from snakemd import Document
 
 # Load environment variables
 load_dotenv('secrets.env')
@@ -14,7 +16,7 @@ zot = Zotero(library_id=os.environ["LIBRARY_ID"], library_type="user", api_key=o
 all_items = zot.everything(zot.all_top())
 
 all_annotations = zot.everything(zot.items(itemType="annotation"))
-
+all_notes = zot.everything(zot.items(itemType="note"))
 
 COLORS = dict(
     red="#ff6666",
@@ -26,45 +28,78 @@ COLORS = dict(
 HEX_to_COLOR = {v: k for k, v in COLORS.items()}
 
 
-class ZoteroItem:
-    pass
-
-@dataclass
-class Annotation:
-    """
-    Except for the parentItem, all other argument name and type are the same as the Zotero API parameters.
-    """
-    parentItem: ZoteroItem
-    version: int
-    itemType: str
-    annotationType: str
-    annotationText: str
-    annotationComment: str
-    annotationColor: str
-    annotationPageLabel: str
-    annotationSortIndex: str
-    annoationPosition: str
-    tags: List
-    dateAdded: str
-    dateModified: str
-
-
-
-with open('all_items.json', 'w') as f:
-    json.dump(all_items, f)
-
+with open('all_annotations.json', 'w') as f:
+    json.dump(all_annotations, f)
 #
-# with open('all_annotations.json', 'r') as f:
-#     data = json.load(f)
+with open('all_annotations.json', 'r') as f:
+    data = json.load(f)
 
-annot_by_parent = defaultdict(list)
+def group_annotations_by_parent_file(annotations: List[Dict]) -> defaultdict:
+    annotations_by_parent = defaultdict(list)
+    for annot in annotations:
+        annotations_by_parent[annot["data"]["parentItem"]].append(annot)
+    return annotations_by_parent
 
-for annot in all_annotations:
-    parent_id = annot["data"]["parentItem"]
-    annot_by_parent[parent_id].append(annot)
 
 counts = {}
 for item in all_items:
     item_type = item['data']['itemType']
     counts[item_type] = counts.get(item_type, 0) + 1
 
+
+
+def format_highlight(highlight: Dict) -> str:
+    data = highlight['data']
+    # zotero_unique_id = f"(key={highlight['key']}, version={highlight['version']})"
+    return (
+        f"{data['annotationText']} "
+        f"({data['annotationPageLabel']})" 
+        f"<!--(Highlighted on {data['dateAdded']})-->"
+        # f"<!---->"
+    )
+
+
+def create_highlights_section(doc: Document, highlights: List) -> None:
+    doc.add_header(level=1, text="Highlights")
+    
+    doc.add_unordered_list(
+        format_highlight(h) for h in highlights
+    )
+
+
+
+d = group_annotations_by_parent_file(all_annotations)
+# item_key = "UIBHCUP6"
+item_key = 'N69RPVEF'
+item_highlights = d[item_key]
+item_details = zot.item(item_key)
+
+def get_item_metadata(item_details: Dict) -> Dict:
+    if "parentItem" in item_details['data']:
+        top_parent_item = zot.item(item_details['data']['parentItem'])
+        data = top_parent_item["data"]
+        metadata = {
+            "title": data["title"],
+            "date": data["date"],
+            "creators": [creator["firstName"] + " " + creator["lastName"] for creator in data["creators"]],
+            "tags": data["tags"],
+        }
+    else:
+        data = item_details['data']
+        metadata = {
+            "title": data["title"],
+            "tags": data["tags"],
+        }
+
+    return metadata
+
+metadata = get_item_metadata(item_details)
+
+# def create_md_doc(highlights, notes, metadata, frontmatter) -> None:
+
+doc = Document(metadata["title"])
+
+# doc.add_element()
+# doc.add_header(level=1, text="Metadata")
+
+create_highlights_section(doc, item_highlights)
