fdef1a2d0348b7de1ea805bc70c1dda6d8180ab6
ealizadeh <alizadeh.essi@gmail.com>
2021-12-28 00:00:39 -0500

    Add some printout when retrieving all items from Zotero db.

--- zotero2md/utils.py
+++ zotero2md/utils.py
@@ -16,8 +16,10 @@ def sanitize_tag(tag: str):
 
 
 def retrieve_all_annotations(zotero_client: Zotero):
+    print("Retrieving ALL annotations from Zotero Database. \nIt may take some time...")
     return zotero_client.everything(zotero_client.items(itemType="annotation"))
 
 
 def retrieve_all_notes(zotero_client: Zotero):
+    print("Retrieving ALL notes from Zotero Database. \nIt may take some time...")
     return zotero_client.everything(zotero_client.items(itemType="note"))
