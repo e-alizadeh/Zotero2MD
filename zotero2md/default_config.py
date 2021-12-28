846367f08b5c9a60c7628dfb2cb16fc9672afbc5
ealizadeh <alizadeh.essi@gmail.com>
2021-12-27 23:13:02 -0500

    Define a default dictionary of params. Rename output_config.json to sample_params.json.

---
+++ zotero2md/default_config.py
@@ -0,0 +1,6 @@
+default_params = {
+    "convertTagsToInternalLinks": True,
+    "doNotConvertFollowingTagsToLink": [],
+    "includeHighlightDate": True,
+    "hideHighlightDateInPreview": False,
+}
