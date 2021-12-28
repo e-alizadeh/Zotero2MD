d08404d70f4c5f0e220c978aa63fbe509ba6291c
ealizadeh <alizadeh.essi@gmail.com>
2021-12-27 21:27:45 -0500

    docs: Add docstrings to sanitize_tag() and sanitize_filename()

--- zotero2md/utils.py
+++ zotero2md/utils.py
@@ -13,11 +13,52 @@ def group_annotations_by_parent_file(annotations: List[Dict]) -> defaultdict:
     return annotations_by_parent
 
 
-def sanitize_tag(tag: str):
+def sanitize_tag(tag: str) -> str:
+    """Clean tag by replacing empty spaces with underscore.
+
+    Parameters
+    ----------
+    tag: str
+
+    Returns
+    -------
+    str
+        Cleaned tag
+
+    Examples
+    --------
+    >>> sanitize_tag(" Machine Learning ")
+    "Machine_Learning"
+
+    """
     return tag.strip().replace(" ", "_")
 
 
-def sanitize_filename(filename: str):
+def sanitize_filename(filename: str) -> str:
+    """Clean filename
+    Replace illegal (or problem-causing) characters with empty spaces, dashes or nothing.
+    char_to_replace dictionary contains the mapping.
+
+    Parameters
+    ----------
+    filename: str
+        original filename
+
+    Returns
+    -------
+    str
+        Sanitized filename (without extension)
+
+    Examples
+    --------
+    >>> sanitize_filename(" abc.pdf")
+    "abc"
+    >>> sanitize_filename("abc:def?ghi")
+    "abc -- ghi"
+    >>> sanitize_filename("abc.def")
+    "abc def"
+
+    """
     out_filename = filename.strip()
     char_to_replace = {".pdf": " ", ":": " -- ", "/": "-", "?": " ", ".": " "}
     for old, new in char_to_replace.items():
